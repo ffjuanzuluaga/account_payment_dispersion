a3d6983a39446183959d132d65f2f607aeb6063d
ffjuanzuluaga <juan.zuluaga@firefly-e.com>
2025-03-27 15:35:40 -0500

    [ADD] cambios vista

--- account_payment_dispersion/__manifest__.py
+++ account_payment_dispersion/__manifest__.py
@@ -15,8 +15,8 @@
     'data': [
         'security/ir.model.access.csv',
         'data/res_bank_data.xml',
-        'views/account_payment_views.xml',
         'views/res_bank_views.xml',
+        'views/account_payment_views.xml',
         'views/account_payment_dispersal_views.xml',
         'views/res_partner_bank_views.xml',
         'views/res_partner_views.xml',
